0be976e2e7e832324d857afcd1c54050a273272b
Reza <rrezai296@gmail.com>
2024-09-21 12:56:56 -0700

    feat: Add connection string configuration and register required services
    - Configured the connection string in the app settings.
    - Registered necessary services in the dependency injection container.

---
+++ CoreApi.Presentation/Controllers/UserController.cs
@@ -0,0 +1,81 @@
+﻿using CoreApi.Entities;
+using Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public async Task<List<User>> Get(CancellationToken cancellationToken)
+        {
+            var users = await userRepository.TableNoTracking.ToListAsync(cancellationToken);
+            return users;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<User>> Get(int id, CancellationToken cancellationToken)
+        {
+            var user = await userRepository.GetByIdAsync(cancellationToken, id);
+            if (user == null)
+                return NotFound();
+            return user;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> Create(User userDto, CancellationToken cancellationToken)
+        {
+            //var exists = await userRepository.TableNoTracking.AnyAsync(p => p.UserName == userDto.UserName);
+            //if (exists)
+            //    return BadRequest("نام کاربری تکراری است");
+
+            var user = new User
+            {
+                Age = userDto.Age,
+                FullName = userDto.FullName,
+                Gender = userDto.Gender,
+                UserName = userDto.UserName
+            };
+            await userRepository.AddAsync(user, userDto.PasswordHash, cancellationToken);
+            return user;
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update(int id, User user, CancellationToken cancellationToken)
+        {
+            var updateUser = await userRepository.GetByIdAsync(cancellationToken, id);
+
+            updateUser.UserName = user.UserName;
+            updateUser.PasswordHash = user.PasswordHash;
+            updateUser.FullName = user.FullName;
+            updateUser.Age = user.Age;
+            updateUser.Gender = user.Gender;
+            updateUser.IsActive = user.IsActive;
+            updateUser.LastLoginDate = user.LastLoginDate;
+
+            await userRepository.UpdateAsync(updateUser, cancellationToken);
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete(int id, CancellationToken cancellationToken)
+        {
+            var user = await userRepository.GetByIdAsync(cancellationToken, id);
+            await userRepository.DeleteAsync(user, cancellationToken);
+
+            return Ok();
+        }
+    }
+}
--- CoreApi.Presentation/Program.cs
+++ CoreApi.Presentation/Program.cs
@@ -1,12 +1,25 @@
+using CoreApi.DataLayer;
+using Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//Add a sql server
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+});
+
+//register service
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
